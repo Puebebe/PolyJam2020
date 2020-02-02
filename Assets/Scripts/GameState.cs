7681861f035031a1335c36020be4cb3c9d44db52
Dirtowladny <dirtowladny@gmail.com>
2020-02-02 12:50:49 +0100

    Fixed Game State

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,6 +1,11 @@
 ﻿public static class GameState
 {
     public static int levelCompleted = 0;
-    public static int socksPairsForLevel = 5 + levelCompleted;
+    public static int socksPairsForLevel {
+        get
+        {
+            return (5 + levelCompleted);
+        }
+    }
     public static int remainingLifes = 5;
 }
