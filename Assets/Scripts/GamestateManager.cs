66dd37ba352488649e0197b6421ecc72b9a2dfad
Wiaderko <bartlomiej.marchlinski@gmail.com>
2020-02-02 14:02:21 +0100

    WhileDev UI elements
    time is almost up

---
+++ Assets/Scripts/GamestateManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public GameObject Visuals;
+    public GameObject GameOverPanel;
+    public GameObject TimeUpPanel;
+    public Camera MainCamera;
+
+
+    private void Awake()
+    {
+        
+    }
+}
