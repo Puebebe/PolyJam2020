00677293aa4807e20a801a6d90a63ee528d3b8e9
Dirtowladny <dirtowladny@gmail.com>
2020-02-02 10:06:59 +0100

    Menu Buttons use Animations as transitions

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,47 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-public class Menu : MonoBehaviour, IPointerEnterHandler
+public class Menu : MonoBehaviour
 {
-    public Button playButton;
-    public Button exitButton;
-    bool isHover;
-    float smooth = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    //public Button playButton;
+    //public Button exitButton;
+    //bool isHover;
+    //float smooth = 100;
 
     // Update is called once per frame
-    void Update()
-    {
-        smooth -= 1f;
-        if (isHover)
-        {
-            smooth = 100;
-            SmoothQuaternion(playButton.gameObject,playButton.transform.rotation, Quaternion.Euler(0, 0, 110));
+    //void Update()
+    //{
+    //    smooth -= 1f;
+    //    if (isHover)
+    //    {
+    //        smooth = 100;
+    //        SmoothQuaternion(playButton.gameObject,playButton.transform.rotation, Quaternion.Euler(0, 0, 110));
 
-            SmoothQuaternion(exitButton.gameObject, exitButton.transform.rotation, Quaternion.Euler(0, 0, 110));
+    //        SmoothQuaternion(exitButton.gameObject, exitButton.transform.rotation, Quaternion.Euler(0, 0, 110));
 
-        }
-        else
-        {
-            SmoothQuaternion(playButton.gameObject,playButton.transform.rotation, Quaternion.Euler(0, 0, 100));
+    //    }
+    //    else
+    //    {
+    //        SmoothQuaternion(playButton.gameObject,playButton.transform.rotation, Quaternion.Euler(0, 0, 100));
 
-            SmoothQuaternion(exitButton.gameObject, exitButton.transform.rotation, Quaternion.Euler(0, 0, 100));
-        }
-    }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        isHover = true;
-        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
-    }
+    //        SmoothQuaternion(exitButton.gameObject, exitButton.transform.rotation, Quaternion.Euler(0, 0, 100));
+    //    }
+    //}
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    isHover = true;
+    //    Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
+    //}
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        isHover = false;
-    }
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    isHover = false;
+    //}
     public void OnClickPlay()
     {
         SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
@@ -53,11 +48,10 @@ public class Menu : MonoBehaviour, IPointerEnterHandler
     {
         Application.Quit();
     }
-    public void SmoothQuaternion(GameObject setTo,Quaternion from, Quaternion to)
-    {
+    //public void SmoothQuaternion(GameObject setTo,Quaternion from, Quaternion to)
+    //{
+    //    smooth = Mathf.Max(smooth, 0);
+    //    setTo.transform.rotation = Quaternion.RotateTowards(from, to, smooth * Time.deltaTime);
 
-        smooth = Mathf.Max(smooth, 0);
-        setTo.transform.rotation = Quaternion.RotateTowards(from, to, smooth * Time.deltaTime);
-
-    }
+    //}
 }
