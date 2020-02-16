0aa36a842376d74c3bdfd8896e5a1bb40a9da579
Unknown <kawaxbart@gmail.com>
2020-02-16 23:53:13 +0100

    Fix reset game state after replay game from menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -43,6 +43,7 @@ public class Menu : MonoBehaviour
     public void OnClickPlay()
     {
         SceneManager.LoadScene("Gameplay_new", LoadSceneMode.Single);
+        GameState.ResetGameState();
     }
     public void OnClickExit()
     {
