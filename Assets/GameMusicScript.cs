209f1ff78ab27c7b5292321f1df0594097744a01
MrsBuckets <45252812+MrsBuckets@users.noreply.github.com>
2020-02-20 09:48:55 +0100

    Music Non Stop
    Nie potrafie skonczyc poziomu zeby sprawdzic czy mumzyka zostaje po powrocie do glownego menu :c

---
+++ Assets/GameMusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicScript : MonoBehaviour
+{
+    public GameObject BackgroundMusicPrefab;
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        GameObject BackgroundMusic = GameObject.Find("BackgroundMusic");
+        if (BackgroundMusic == null)
+        {
+            Instantiate(BackgroundMusicPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            DontDestroyOnLoad(GameObject.Find("BackgroundMusic"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
